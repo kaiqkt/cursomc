92b7ccca2cc0fda36264ba42ff6e13d47a4065bf
kaiquepill <kaique.gomes@bandtec.com.br>
2020-10-08 17:37:45 -0300

    Testando a primeira classe de entidade

--- src/main/java/com/kaiquegomesw/cursomc/controllers/CategoriaController.java
+++ src/main/java/com/kaiquegomesw/cursomc/controllers/CategoriaController.java
@@ -1,15 +1,26 @@
 package com.kaiquegomesw.cursomc.controllers;
 
+import com.kaiquegomesw.cursomc.entities.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
 
     @GetMapping()
-    public String listar() {
-        return "Oii";
+    public List listar() {
+
+        Categoria cat1 = new Categoria(1, "informatica");
+        Categoria cat2 = new Categoria(2, "escritorio");
+
+        List<Categoria> list = new ArrayList();
+
+        list.add(cat1);
+        list.add(cat2);
+        return list;
     }
 }
